7cbfb53b4a29574c3e02bb927137dd1e07a8d1f0
umakant <happydubey09@gmail.com>
2021-08-19 04:38:57 +0530

    defining user model ,route, controllers

---
+++ app/routes/user.routes.js
@@ -0,0 +1,18 @@
+module.exports = (app) => {
+    const notes = require('../controllers/user.controller.js');
+
+    // Create a new User
+    app.post('/users', user.create);
+
+    // Retrieve all User
+    app.get('/users', user.findAll);
+
+    // Retrieve a single User with UserId
+    app.get('/users/:userId', user.findOne);
+
+    // Update a User with UserId
+    app.put('/users/:userId', user.update);
+
+    // Delete a user with userId
+    app.delete('/users/:userId', user.delete);
+}
